a2aa73039c1cdad76c3bea28069df4f1bfc3e8a9
Terry <terry@ind.fm>
2011-01-26 17:17:16 -0500

    changed js directory

--- js/app.js
+++ js/app.js
@@ -1,12 +1,3 @@
-// Instantiating
-  var indfm = new Project({ // attributes passed to the Donut constructor will override the defaults
-    name : "Ind.fm",
-    complete : false
-  });
-
-  // Saving
-  indfm.save(); // this will now POST to the RESTful interface.
-
 $("a").live("click", function(e){
   if(e["stopPropagation"] && e["preventDefault"])
   {
