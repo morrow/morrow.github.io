6f8acee675d54f05c23d55d2a8cee87a293fc9f3
Terry <terry@ind.fm>
2011-01-26 17:13:09 -0500

    added logic to app.js

--- js/app.js
+++ js/app.js
@@ -6,3 +6,17 @@
 
   // Saving
   indfm.save(); // this will now POST to the RESTful interface.
+
+$("a").live("click", function(e){
+  if(e["stopPropagation"] && e["preventDefault"])
+  {
+    e.stopPropagation();
+    e.preventDefault();
+  }
+  else
+  {
+    e.cancelBubble = true;
+    e.returnValue = false;
+  }
+  console.log($(this).attr("href"));
+});
