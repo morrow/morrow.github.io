015728764a0947bbdb2e6d02efeea1dfe1c42da3
Terry <terry@ind.fm>
2011-01-27 15:44:37 -0500

    updated app.js

--- js/app.js
+++ js/app.js
@@ -17,7 +17,7 @@ var load = function(url)
 	uri.display = uri.display.split(".html")[0]
 	if("pushState" in window.history)
 	{
-		if(window.location.hash)
+		if(window.location.hash && window.location.hash.length > 1)
 		{
 			window.history.replaceState({id:uri.display}, uri.display, uri.display);
 
