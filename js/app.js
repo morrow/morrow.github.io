a0df85dd6b54648da729ec1e87890762d12aad08
Terry <terry@ind.fm>
2011-01-27 14:41:55 -0500

    updated app.js

--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@ var load = function(url)
 {
 	if("pushState" in window.history)
 	{
-		if(Math.abs(uri.history.indexOf(url)) - Math.abs(uri.history.indexOf(uri.current)) <= 1 || window.location.hash)
+		if(uri.history.indexOf(url) == uri.history.indexOf(uri.current)) - 1 || (uri.history.indexOf(url) == uri.history.indexOf(uri.current)) + 1 || window.location.hash)
 		{
 			window.history.replaceState({id:url}, url, url);
 
