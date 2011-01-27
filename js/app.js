c9e2ec5a48b8b265b5b08aea5f5276ca0770c9a3
Terry <terry@ind.fm>
2011-01-27 14:17:11 -0500

    updated app.js

--- js/app.js
+++ js/app.js
@@ -13,7 +13,7 @@ var load = function(url)
 	uri.display = uri.display.split("/")[1];
 	if("pushState" in window.history)
 	{
-		if(uri.history.indexOf(url) == uri.history.length - 2)
+		if(uri.history.indexOf(url) == uri.history.length - 2 || window.location.hash)
 		{
 			window.history.replaceState({id:url}, url, url);
 		}
