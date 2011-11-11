7880a0b91cbd57d7ef3c15dbe66953e35105e968
Terry Morrow <terrymorrow@gmail.com>
2011-11-10 22:38:05 -0500

    moved jquery to local file

--- javascripts/data.js
+++ javascripts/data.js
@@ -3,7 +3,6 @@ data = {
   document: {
     "root": {
       "h1": "Terry Morrow",
-      "h2": "Projects",
       about: {
         category: {
           icon: "i",
@@ -24,6 +23,23 @@ data = {
             name: "KISSmetrics",
             website: "https://www.kissmetrics.com/",
             description: "Engagement data made easy. Learn faster, grow your business, and make smarter decisions."
+          }
+        }
+      },
+      about: {
+        category: {
+          icon: "i",
+          name: "Projects"
+        },
+        items: {
+          "1": {
+            logo: "./images/leftronic.png",
+            name: "Leftronic",
+            website: "https://beta.leftronic.com/main/",
+            description: "Our software makes it incredibly easy to deploy powerful dashboards to track the data that matters most to your company.<br /> Whether you're tracking web engagement, sales, user activity, or server stats, our dashboards keep you up-to-date.",
+            links: {
+              hnsearch: "http://hnsearch.com/search?q=leftronic"
+            }
           }
         }
       }
